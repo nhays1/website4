597ba54bb65acf609b96662feb13cfcf9a33fb9b
Nate <nhays@ycp.edu>
2018-03-15 20:34:44 -0400

    more juice in the userTest class

--- junit/edu/ycp/cs320/jminor1/servlet/userTest.java
+++ junit/edu/ycp/cs320/jminor1/servlet/userTest.java
@@ -4,11 +4,25 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import website4.model.usser;
+
 public class userTest {
+	
+	private usser user1;
+	private String username;
+	private String password;
+	private usser userGuest;
 	
 	@Before
 	public void setup() throws Exception{
-		
+		username =  "usernameXD";
+		password = "passwordXD";
+		user1 = new usser(username, password);
+	}
+	
+	@Test
+	public void testGenericUser() {
+		assertTrue(user1.getcoins() == 0);
 	}
 	
 }
