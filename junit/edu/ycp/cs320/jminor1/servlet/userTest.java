a353f6cc2d03ff8076f47c710581b33a841f8342
Nate <nhays@ycp.edu>
2018-03-15 20:14:24 -0400

    userTest added

--- junit/edu/ycp/cs320/jminor1/servlet/userTest.java
+++ junit/edu/ycp/cs320/jminor1/servlet/userTest.java
@@ -1,5 +1,14 @@
 package edu.ycp.cs320.jminor1.servlet;
 
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
 public class userTest {
+	
+	@Before
+	public void setup() throws Exception{
+		
+	}
 	
 }
