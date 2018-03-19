06e2fd4df8976c67cd0d4f4b7b3917696f7788c3
Nate <nhays@ycp.edu>
2018-03-19 00:26:14 -0400

    Updated full User JUnit test

--- junit/edu/ycp/cs320/jminor/model/CreateUserTest.java
+++ junit/edu/ycp/cs320/jminor/model/CreateUserTest.java
@@ -1,6 +1,14 @@
 package edu.ycp.cs320.jminor.model;
 
 import static org.junit.Assert.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.junit.Before;
 import org.junit.Test;
--- junit/edu/ycp/cs320/jminor/model/userTest.java
+++ junit/edu/ycp/cs320/jminor/model/userTest.java
@@ -1,6 +1,9 @@
 package edu.ycp.cs320.jminor.model;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -12,21 +15,56 @@ public class userTest {
 	private String username;
 	private String password;
 	private usser userGuest;
+	private usser userGuest2;
 	String email = "email";
+
+
+	private ArrayList<usser> bList;
 	
 	@Before
 	public void setup() throws Exception{
 		username =  "usernameXD";
 		password = "passwordXD";
 		user1 = new usser(username, password);
+		bList = new ArrayList<usser>();
 	}
 	
 	@Test
-	public void testGenericUser() { //tests real user constructor
+	public void testGenericUser() { //test test
 		assertTrue(user1.getcoins() == 0);
 		assertTrue(user1.isguest() == false);
 		assertTrue(user1.getusername() == "usernameXD");
 		assertTrue(user1.getpassword() == "passwordXD");
 	}
 	
+	@Test
+	public void testGuestUser() {
+		
+		for(int i = 0; i < 1000; i++) {
+			userGuest = new usser();
+			userGuest2 = new usser();
+			assertTrue(userGuest.getusername() != userGuest2.getusername());
+		}
+	
+		assertTrue(userGuest.getcoins() == 0);
+		assertTrue(userGuest.isguest() == true);
+		assertTrue(userGuest2.getcoins() == 0);
+		assertTrue(userGuest2.isguest() == true);
+		assertTrue(userGuest.getusername() != userGuest2.getusername());
+	}
+	
+	@Test
+	public void testGettersSetters(){
+		bList.add(userGuest);
+		user1.setcoins(100);
+		user1.setemail("theDude@gmail.gov");
+		user1.setpassword("doggy");
+		user1.setusername("itsYaBoi69");
+		user1.addtoblacklist(userGuest);
+		assertTrue(user1.getblacklist().get(0) == bList.get(0));
+		assertTrue(user1.getcoins() == 100);
+		assertTrue(user1.getemail() == "theDude@gmail.gov");
+		assertTrue(user1.getpassword() == "doggy");
+		assertTrue(user1.getusername() == "itsYaBoi69");
+	}
 }
