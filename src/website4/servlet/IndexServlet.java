d48eadf4da6298509b382ec5599c204bfd0f53db
minor <jminor1@ycp.edu>
2018-03-23 13:24:55 -0400

    added gson library, and chat controler/ model

--- src/website4/servlet/IndexServlet.java
+++ src/website4/servlet/IndexServlet.java
@@ -1,11 +1,19 @@
 package website4.servlet;
 
 import java.io.IOException;
+import java.time.Instant;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.GsonBuilder;
+import com.google.gson.Gson;
+
+import website4.controller.chatcontroler;
+import website4.model.post;
 
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -15,7 +23,20 @@ public class IndexServlet extends HttpServlet {
 			throws ServletException, IOException {
 		
 		System.out.println("index Servlet: doGet");
+		int chatlength;
+		chatcontroler chat =new chatcontroler();
+		ArrayList<post> chatposts;
+		chatposts= chat.Getchat();
 		
+		chatlength=chatposts.size();
+		
+		Gson gson = new GsonBuilder().create();
+		String jsonchstpost = gson.toJson(chatposts);
+		
+		//System.out.println("jsonobj      _"+jsonchstpost);
+		
+		req.setAttribute("chatposts", jsonchstpost);
+		req.setAttribute("chatlength", chatlength);
 		req.getRequestDispatcher("/_view/index.jsp").forward(req, resp);
 	}
 	
@@ -25,7 +46,50 @@ public class IndexServlet extends HttpServlet {
 			throws ServletException, IOException {
 		
 		System.out.println("index Servlet: doPost");
+		int chatlength;
+		chatcontroler chat =new chatcontroler();
 		
+		try {
+			
+			
+			String chatinput =  req.getParameter("chatinputtext");
+			
+			System.out.println("chat      _ "+chatinput);
+			if(chatinput!=null) {
+				if(chatinput.trim().length()>0) {
+					long now=Instant.now().toEpochMilli();
+					chat.makenewpost(now, "jmino", chatinput);
+					System.out.println("chatpassed      _ "+chatinput);
+					
+					
+					
+					
+					
+				}
+			}
+			
+			
+
+		}
+		finally{
+			
+		}
+		
+		
+
+		
+		ArrayList<post> chatposts;
+		chatposts= chat.Getchat();
+		
+		chatlength=chatposts.size();
+		
+		Gson gson = new GsonBuilder().create();
+		String jsonchstpost = gson.toJson(chatposts);
+		
+		//System.out.println("jsonobj      _"+jsonchstpost);
+		
+		req.setAttribute("chatposts", jsonchstpost);
+		req.setAttribute("chatlength", chatlength);
 		req.getRequestDispatcher("/_view/index.jsp").forward(req, resp);
 	}
 }
