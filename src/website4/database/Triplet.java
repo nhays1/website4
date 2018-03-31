a874f8ba45dcc2676590ad2d5f41e5686b8f98b4
Jason Pierpont <jpierpont@ycp.edu>
2018-03-31 17:51:07 -0400

    Added triplet class for FakeDatabase purposes

---
+++ src/website4/database/Triplet.java
@@ -0,0 +1,24 @@
+package website4.database;
+
+public class Triplet <T, U, V> {
+	
+
+	    private final T first;
+	    private final U second;
+	    private final V third;
+
+	    public Triplet(T first, U second, V third) {
+	        this.first = first;
+	        this.second = second;
+	        this.third = third;
+	    }
+
+	    public T getFirst() { return first; }
+	    public U getSecond() { return second; }
+	    public V getThird() { return third; }
+	}
+
+//This code was taken from stackoverflow by username Osguima3
+
+//https://stackoverflow.com/questions/6010843/java-how-to-store-data-triple-in-a-list?utm_medium=or
+//ganic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
