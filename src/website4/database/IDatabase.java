86d690bbeacb26669a0308bcc636e1e32c157d66
Jason Pierpont <jpierpont@ycp.edu>
2018-04-05 11:36:57 -0400

    Added createUser and isValid methods

--- src/website4/database/IDatabase.java
+++ src/website4/database/IDatabase.java
@@ -1,5 +1,6 @@
 package website4.database;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,4 +13,6 @@ public interface IDatabase {
 	public List<post> getposts_no_blacklist(int chatindex,int numposts);
 	public void addpost(long mils_time ,int userid,String posttext);
 	public usser getuser_by_id(int id);
+	public void createUser(String userName, String password, String email)throws SQLException;
+	public boolean isValid(final String userName, String password,final String email) throws SQLException;
 }
